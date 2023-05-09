6f85044dd2f1f34114c8a23d1e545af6978861fb
Bizio00 <129618410+Bizio00@users.noreply.github.com>
2023-05-09 19:10:42 +0200

    modifiche generiche
    gestione statua zeus
    modifica salto
    inserimento arma di prova

--- Assets/Script/Player_controller.cs
+++ Assets/Script/Player_controller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player_controller : MonoBehaviour
 {
     private CharacterController ch;
-   
+    public GameObject gos;
     Vector3 movement;
 
     //Parametri di movimento
@@ -13,22 +13,21 @@ public class Player_controller : MonoBehaviour
     float Vertical_mov;
     public float speed;
     public float rotationSpeed;
-    private const float jumpHeight = 2f;
     private const float gravity = 9.81f;
     public float vspeed=0;
     public float Knock_Back_Force = 1.0f;
     public float Knock_Back_Time;
     private const float Knock_Back_Max_Time = 1.0f;
-    
-    
+    public float jump_force;
+    public int score = 0;
 
     void Start()
     {
-        
+
         
         Cursor.lockState = CursorLockMode.Locked;
         ch = GetComponent<CharacterController>();
-        
+        gos.SetActive(false);
         
     }
 
@@ -52,11 +51,12 @@ public class Player_controller : MonoBehaviour
                 if (vspeed < 0)
                     vspeed = -2f;
 
-                //float jump = Input.GetAxis("Jump");
+                float jump = Input.GetAxis("Jump");
                 if (Input.GetButtonDown("Jump"))
                 {
 
-                    vspeed += Mathf.Sqrt(jumpHeight * 2 * gravity);    
+                    
+                    vspeed = jump * jump_force;
                 }
 
 
@@ -81,6 +81,10 @@ public class Player_controller : MonoBehaviour
 
         ch.transform.Rotate(Vector3.up * Horizontal_mov);
         ch.Move(movement);
+        if (score == 5)
+        {
+            gos.SetActive(true);
+        }
     }
         
 
@@ -105,15 +109,22 @@ public class Player_controller : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+
+        
         if (other.CompareTag("Collect"))
         {
             
             other.gameObject.SetActive(false);
-            
+            score+=1;
            
 
 
         }
+        
+            
+           
+            
+           
         
     }
 }
