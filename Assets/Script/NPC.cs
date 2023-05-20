7e56bb2fb33adadaca0f20e76cc62d7dc6ac0c6e
Chiara <chiara.ganga98@gmail.com>
2023-05-20 20:23:25 +0200

    dialogo
    nuova versione (temporanea) del dialogo

--- Assets/Script/NPC.cs
+++ Assets/Script/NPC.cs
@@ -3,106 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class NPC : MonoBehaviour
 {
     public GameObject dialogPanel;
+    private GameObject Zeus;
     public TMP_Text dialogueText;
     public string[] dialogo;
     private bool isWriting = false;
 
     private bool isDialogActive = false;
     private bool isFirstDialog = true;
+    private bool isPlayerClose = false;
 
     private int index;
     public float velocityword;
-    public bool playerIsClose;
+
+    // Tempo di visualizzazione del messaggio "Ciao, ti devo parlare!"
+    public float initialMessageDuration = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        dialogPanel.SetActive(false);
+        Zeus = GameObject.FindGameObjectWithTag("Zeus");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        if (isFirstDialog && Zeus != null && Zeus.activeSelf)
         {
-            if (dialogPanel.activeInHierarchy)
-            {
-                nienteTesto();
-            }
-            else
-            {
-                dialogPanel.SetActive(true);
-                isDialogActive = true;
-                StartCoroutine(Scrittura());
-            }
-        }
-
-        if (isDialogActive && !isWriting && Input.anyKeyDown)
-        {
-            NuovaLinea();
-        }
-    }
-
-    public void nienteTesto()
-    {
-        dialogueText.text = "";
-        index = 0;
-        dialogPanel.SetActive(false);
-        isDialogActive = false;
-        isFirstDialog = true;
-    }
-
-    IEnumerator Scrittura()
-    {
-        isWriting = true;
-
-        foreach (char letter in dialogo[index].ToCharArray())
-        {
-            dialogueText.text += letter;
-            yield return new WaitForSeconds(velocityword);
-        }
-
-        isWriting = false;
-    }
-
-    public void NuovaLinea()
-    {
-        if (isWriting)
-        {
-            return;
-        }
-
-        if (isFirstDialog)
-        {
+            // Mostra il messaggio iniziale e avvia il conteggio alla fine del quale scompare
+            StartDialog("Ciao, ti devo parlare!");
             isFirstDialog = false;
-            index = 1;
-        }
-        else
-        {
-            if (index < dialogo.Length - 1)
-            {
-                index++;
-            }
-            else
-            {
-                nienteTesto();
-                return;
-            }
+            Invoke("EndInitialDialog", initialMessageDuration);
         }
 
-        dialogueText.text = "";
-        StartCoroutine(Scrittura());
+        if (isDialogActive)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && isPlayerClose)
+            {
+                if (isWriting)
+                {
+                    CompleteWriting();
+                }
+                else
+                {
+                    if (index < dialogo.Length - 1)
+                    {
+                        index++;
+                        StartWriting(dialogo[index]);
+                    }
+                    else
+                    {
+                        EndDialog();
+                    }
+                }
+            }
+        }
+        else if (isPlayerClose && Input.GetKeyDown(KeyCode.E))
+        {
+            StartDialog(dialogo[index]);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            playerIsClose = true;
+            isPlayerClose = true;
         }
     }
 
@@ -110,8 +81,53 @@ public class NPC : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            playerIsClose = false;
-            nienteTesto();
+            isPlayerClose = false;
         }
+    }
+
+    private void StartDialog(string message)
+    {
+        isDialogActive = true;
+        dialogPanel.SetActive(true);
+        StartWriting(message);
+    }
+
+    private void EndDialog()
+    {
+        isDialogActive = false;
+        dialogPanel.SetActive(false);
+        index = 0;
+    }
+
+    private void StartWriting(string message)
+    {
+        dialogueText.text = "";
+        StartCoroutine(WriteText(message));
+    }
+
+    private void CompleteWriting()
+    {
+        StopAllCoroutines();
+        dialogueText.text = dialogo[index];
+        isWriting = false;
+    }
+
+    private IEnumerator WriteText(string message)
+    {
+        isWriting = true;
+
+        for (int i = 0; i < message.Length; i++)
+        {
+            dialogueText.text += message[i];
+            yield return new WaitForSeconds(velocityword);
+        }
+
+        isWriting = false;
+    }
+
+    // Metodo chiamato dopo il tempo di visualizzazione del messaggio iniziale
+    private void EndInitialDialog()
+    {
+        EndDialog();
     }
 }
