9850fd0f41c8ae3d9a92a93f2e4ee11a7659160f
Chiara <chiara.ganga98@gmail.com>
2023-05-22 20:24:42 +0200

    commenti codice NPC
    aggiunti commenti al codice

--- Assets/Script/NPC.cs
+++ Assets/Script/NPC.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Runtime.ConstrainedExecution;
 
 public class NPC : MonoBehaviour
 {
+    //Variabili
     public GameObject dialogPanel;
     private GameObject Zeus;
     public TMP_Text dialogueText;
@@ -23,10 +25,10 @@ public class NPC : MonoBehaviour
     // Tempo di visualizzazione del messaggio iniziale
     public float initialMessageDuration = 5f;
 
-    // Durata del secondo dialogo dopo aver premuto E (4 secondi)
+    // Durata del secondo dialogo (3 secondi)
     public float secondDialogDuration = 3f;
 
-    // Durata del terzo dialogo dopo aver premuto E (8 secondi)
+    // Durata del terzo dialogo (7 secondi)
     public float thirdDialogDuration = 7f;
 
     // Start is called before the first frame update
@@ -47,12 +49,14 @@ public class NPC : MonoBehaviour
             Invoke("EndInitialDialog", initialMessageDuration);
         }
 
-        if (isDialogActive)
+        if (isDialogActive) //controllo che il dialogo sia attivo
         {
-            if (isWriting)
+            if (isWriting) //controllo se sta scrivendo
             {
                 if (Time.time >= initialMessageDuration)
                 {
+                    //Se il tempo di gioco supera o è uguale alla durata del messaggio iniziale,
+                    //completo la scrittura del testo.
                     CompleteWriting();
                 }
             }
@@ -60,21 +64,22 @@ public class NPC : MonoBehaviour
             {
                 if (Time.time >= initialMessageDuration)
                 {
-                    if (index < dialogo.Length - 1)
+                    if (index < dialogo.Length - 1) //Se l'indice è inferiore al numero totale di messaggi incremento di 1
                     {
                         index++;
-                        StartWriting(dialogo[index]);
+                        StartWriting(dialogo[index]); //Scrivo nuovo messaggio
                         initialMessageDuration = Time.time + velocityword * dialogo[index].Length;
 
                         if (index == dialogo.Length - 1)
                         {
+                            //Se l'indice raggiunge l'ultimo messaggio nel dialogo, viene chiuso il dialogo.
                             Invoke("EndDialog", thirdDialogDuration);
                         }
                     }
                 }
             }
         }
-        else if (isPlayerClose && Input.GetKeyDown(KeyCode.E))
+        else if (isPlayerClose && Input.GetKeyDown(KeyCode.E)) //Se il player e' vicino e ha premuto E
         {
             StartDialog(dialogo[index]);
             initialMessageDuration = Time.time + secondDialogDuration;
@@ -90,7 +95,7 @@ private void OnTriggerEnter(Collider other)
         }
     }
 
-    private void OnTriggerExit(Collider other)
+private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
@@ -98,53 +103,55 @@ private void OnTriggerEnter(Collider other)
         }
     }
 
-    private void StartInitialDialog(string message)
+private void StartInitialDialog(string message)
     {
         isInitialDialogActive = true;
         dialogPanel.SetActive(true);
         dialogueText.text = message;
     }
 
-    private void EndInitialDialog()
+private void EndInitialDialog()
     {
         isInitialDialogActive = false;
         dialogPanel.SetActive(false);
     }
 
-    private void StartDialog(string message)
+private void StartDialog(string message)
     {
         isDialogActive = true;
         dialogPanel.SetActive(true);
         StartWriting(message);
     }
 
-    private void EndDialog()
+private void EndDialog()
     {
         isDialogActive = false;
         dialogPanel.SetActive(false);
         index = 0;
     }
 
-    private void StartWriting(string message)
+private void StartWriting(string message)
     {
         dialogueText.text = "";
         StartCoroutine(WriteText(message));
     }
 
-    private void CompleteWriting()
+private void CompleteWriting()
     {
         StopAllCoroutines();
         dialogueText.text = dialogo[index];
         isWriting = false;
     }
 
-    private IEnumerator WriteText(string message)
+private IEnumerator WriteText(string message)
     {
         isWriting = true;
 
+        //Concatenazione
         for (int i = 0; i < message.Length; i++)
         {
             dialogueText.text += message[i];
+            //Gestione della pausa
             yield return new WaitForSeconds(velocityword);
         }
 
