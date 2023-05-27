a6ce73596228a5173d4ed1de279478b34b524ff2
Chiara <chiara.ganga98@gmail.com>
2023-05-27 10:25:48 +0200

    modifiche dialogo
    modifiche dialogo

--- Assets/Script/NPC_Dialogue.cs
+++ Assets/Script/NPC_Dialogue.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using System.Runtime.ConstrainedExecution;
 
 public class NPC_Dialogue : MonoBehaviour
 {
-    //Variabili
     public GameObject dialogPanel;
     private GameObject Zeus;
     public TMP_Text dialogueText;
     public string[] dialogo;
     private bool isWriting = false;
-    // private bool isInitialDialogActive = false;
 
     private bool isDialogActive = false;
     private bool isFirstDialog = true;
@@ -22,70 +19,44 @@ public class NPC_Dialogue : MonoBehaviour
     private int index;
     public float velocityword;
 
-    // Tempo di visualizzazione del messaggio iniziale
     public float initialMessageDuration = 5f;
+    public float delayBetweenMessages = 7f;
 
-    // Durata del secondo dialogo (3 secondi)
-    public float secondDialogDuration = 3f;
-
-    // Durata del terzo dialogo (7 secondi)
-    public float thirdDialogDuration = 7f;
-
-    // Start is called before the first frame update
     void Start()
     {
         dialogPanel.SetActive(false);
         Zeus = GameObject.FindGameObjectWithTag("Zeus");
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (isFirstDialog && Zeus != null && Zeus.activeSelf)
         {
-            // Mostra il messaggio iniziale e avvia il conteggio alla fine del quale scompare
             StartInitialDialog("Ercole! Grazie al cielo hai aggiustato la mia statua! Avvicinati, ti devo parlare.");
             isFirstDialog = false;
             Invoke("EndInitialDialog", initialMessageDuration);
         }
 
-        if (isDialogActive) //controllo che il dialogo sia attivo
+        if (isDialogActive)
         {
-            if (isWriting) //controllo se sta scrivendo
+            if (isWriting)
             {
                 if (Time.time >= initialMessageDuration)
                 {
-                    //Se il tempo di gioco supera o è uguale alla durata del messaggio iniziale,
-                    //completo la scrittura del testo.
                     CompleteWriting();
-                }
-            }
-            else
-            {
-                if (Time.time >= initialMessageDuration)
-                {
-                    if (index < dialogo.Length - 1) //Se l'indice è inferiore al numero totale di messaggi incremento di 1
-                    {
-                        index++;
-                        StartWriting(dialogo[index]); //Scrivo nuovo messaggio
-                        initialMessageDuration = Time.time + velocityword * dialogo[index].Length;
-
-                        if (index == dialogo.Length - 1)
-                        {
-                            //Se l'indice raggiunge l'ultimo messaggio nel dialogo, viene chiuso il dialogo.
-                            Invoke("EndDialog", thirdDialogDuration);
-                        }
-                    }
+                    Invoke("StartNextMessage", delayBetweenMessages);
                 }
             }
         }
-        else if (isPlayerClose && Input.GetKeyDown(KeyCode.E)) //Se il player e' vicino e ha premuto E
+        else if (isPlayerClose && Input.GetKeyDown(KeyCode.E))
         {
-            StartDialog(dialogo[index]);
-            initialMessageDuration = Time.time + secondDialogDuration;
+            if (index < dialogo.Length)
+            {
+                StartDialog(dialogo[index]);
+                initialMessageDuration = Time.time + velocityword * dialogo[index].Length;
+            }
         }
     }
-
 
     private void OnTriggerEnter(Collider other)
     {
@@ -105,14 +76,12 @@ public class NPC_Dialogue : MonoBehaviour
 
     private void StartInitialDialog(string message)
     {
-        //  isInitialDialogActive = true;
         dialogPanel.SetActive(true);
         dialogueText.text = message;
     }
 
     private void EndInitialDialog()
     {
-        // isInitialDialogActive = false;
         dialogPanel.SetActive(false);
     }
 
@@ -147,14 +116,28 @@ public class NPC_Dialogue : MonoBehaviour
     {
         isWriting = true;
 
-        //Concatenazione
         for (int i = 0; i < message.Length; i++)
         {
             dialogueText.text += message[i];
-            //Gestione della pausa
             yield return new WaitForSeconds(velocityword);
         }
 
+        yield return new WaitForSeconds(0.5f);
+
         isWriting = false;
+    }
+
+    private void StartNextMessage()
+    {
+        index++;
+        if (index < dialogo.Length)
+        {
+            StartDialog(dialogo[index]);
+            initialMessageDuration = Time.time + velocityword * dialogo[index].Length;
+        }
+        else
+        {
+            EndDialog();
+        }
     }
 }
