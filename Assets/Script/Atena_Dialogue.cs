0cd7e047d1f3382db78a8cca7ca3737921b90921
Chiara <chiara.ganga98@gmail.com>
2023-06-09 18:06:52 +0200

    dialoghi
    dialoghi livello 2

--- Assets/Script/Atena_Dialogue.cs
+++ Assets/Script/Atena_Dialogue.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class Atena_Dialogue : MonoBehaviour
 {
@@ -12,7 +13,22 @@ public class Atena_Dialogue : MonoBehaviour
     public string[] dialogo;
     private bool isWriting = false;
 
+    private bool isDialogActive = false;
+    private bool isFirstDialog = true;
+    private bool isPlayerClose = false;
+
+    private int index;
+    public float velocityword;
+
+    public float initialMessageDuration = 5f;
+    public float delayBetweenMessages = 7f;
+
+    public GameObject objectToShow;
+
     public QuizManager quizManager;
+
+    private bool isFirstMessageShown = false;
+    private bool isLastMessage = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +40,132 @@ public class Atena_Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlayerClose && (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Joystick1Button1)))
+        {
+            if (!isDialogActive)
+            {
+                if (isFirstDialog)
+                {
+                    StartInitialDialog(dialogo[index]);
+                    isFirstDialog = false;
+                    Invoke("ShowNextMessage", initialMessageDuration);
+                }
+                else if (isFirstMessageShown)
+                {
+                    Invoke("ShowNextMessage", initialMessageDuration);
+                }
+            }
+            else if (isWriting)
+            {
+                CompleteWriting();
+                Invoke("StartNextMessage", delayBetweenMessages);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerClose = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerClose = false;
+        }
+    }
+
+    private void StartInitialDialog(string message)
+    {
+        dialogPanel.SetActive(true);
+        dialogueText.text = message;
+    }
+
+    private void EndInitialDialog()
+    {
+        dialogPanel.SetActive(false);
+    }
+
+    private void StartDialog(string message)
+    {
+        isDialogActive = true;
+        dialogPanel.SetActive(true);
+        StartWriting(message);
+    }
+
+    private void EndDialog()
+    {
+        isDialogActive = false;
+        if (isLastMessage)
+        {
+            dialogPanel.SetActive(false);
+        }
+        index = 0;
+
+        if (objectToShow != null)
+        {
+            objectToShow.SetActive(true);
+        }
+    }
+
+    private void StartWriting(string message)
+    {
+        dialogueText.text = "";
+        StartCoroutine(WriteText(message));
+    }
+
+    private void CompleteWriting()
+    {
+        StopAllCoroutines();
+        dialogueText.text = dialogo[index];
+        isWriting = false;
+    }
+
+    private IEnumerator WriteText(string message)
+    {
+        isWriting = true;
+
+        for (int i = 0; i < message.Length; i++)
+        {
+            dialogueText.text += message[i];
+            yield return new WaitForSeconds(velocityword);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        isWriting = false;
+    }
+
+    private void ShowNextMessage()
+    {
+        index++;
+        if (index < dialogo.Length)
+        {
+            StartDialog(dialogo[index]);
+            initialMessageDuration = Time.time + velocityword * dialogo[index].Length;
+            isFirstMessageShown = true;
+        }
+        else
+        {
+            isLastMessage = true;
+            Invoke("EndDialog", 5f); // Chiude il dialogo dopo 5 secondi
+        }
+    }
+
+    private void StartNextMessage()
+    {
+        ShowNextMessage();
     }
 
     private void QuizStartDialogue()
     {
         Debug.Log("funziona"); //testing
     }
+
     private void QuizEndDialogue()
     {
         if (quizManager.score == 3)
