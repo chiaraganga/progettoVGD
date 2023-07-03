104ebcda6cd355e6c23c8e3625470de873b8fb22
albimann <133178849+albimann@users.noreply.github.com>
2023-07-03 16:22:38 +0200

    Modifica del comportamento dei soldati nel livello 1
    Adesso i soldati rimangono in uno stato di Idle finche non entro all'interno di un range di inseguimento. Non ruotano più su loro stessi, mi guardano quando mi inseguono e anche quando mi attaccano. Durante l'attacco stanno fermi sul posto e poi riprendono l'inseguimento.

--- Assets/Script/EnemyHandler.cs
+++ Assets/Script/EnemyHandler.cs
@@ -5,69 +5,86 @@ using UnityEngine.AI;
 
 public class EnemyHandler : MonoBehaviour
 {
-    public GameObject player, enemy; // Assegna qui il tuo giocatore.
+    public GameObject player; // Assegna qui il tuo giocatore.
     private NavMeshAgent agent;
     private Animator animator;
-    private float attackDistance = 1.5f; // Definisci la distanza a cui il nemico deve essere per attaccare. Modifica questo valore se necessario.
-    private float idleDistance = 10f; // Definisci la distanza a cui il nemico entra nello stato di riposo. Modifica questo valore se necessario.
-
-    private float lookAtWeight = 1.0f;
-
-    private bool isChasing = false;
+    private float attackDistance = 2.5f; // Definisci la distanza a cui il nemico deve essere per attaccare. Modifica questo valore se necessario.
+    private float stoppingDistance = 3f; // Definisci la distanza a cui il nemico si ferma per attaccare. Modifica questo valore se necessario.
+    private bool isAttacking = false; // Stato di attacco del nemico
+    private float idleDistance = 10f; // Definisci la distanza a cui il nemico entra in stato di riposo. Modifica questo valore se necessario.
 
     void Start()
     {
-        // Assicurati che il GameObject abbia un NavMeshAgent e un Animator.
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
 
         if (agent == null || animator == null || player == null)
         {
-            Debug.LogError("Componente critico mancante dal nemico o giocatore non assegnato.");
+            Debug.LogError("Componenti critici mancanti dal nemico o il giocatore non è stato assegnato.");
             return;
         }
 
-        agent.stoppingDistance = attackDistance;
+        agent.stoppingDistance = stoppingDistance;
+        agent.obstacleAvoidanceType = ObstacleAvoidanceType.NoObstacleAvoidance; // Disabilita l'evitamento degli ostacoli
     }
 
     void Update()
+{
+    if (player != null)
     {
-        if (player != null)
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+
+        if (distanceToPlayer > stoppingDistance)
         {
-             agent.transform.LookAt(player.transform.position);
-            // Calcola la distanza dal giocatore
-            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
-
-            if (distanceToPlayer <= idleDistance)
+            if (!isAttacking && distanceToPlayer > idleDistance)
             {
-                // Insegue il giocatore.
+                agent.enabled = false;
+                animator.SetBool("grounded", true); // Si assume che il nemico sia sempre a terra durante l'Idle
+                animator.SetBool("attack", false); // Non attacca durante l'Idle
+                animator.SetFloat("Velocity", 0f); // Nessuna velocità durante l'Idle
+
+                // Blocca la rotazione sull'asse X e Z
+                transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
+            }
+            else
+            {
+                agent.enabled = true;
                 agent.SetDestination(player.transform.position);
-                isChasing = true;
 
-                // Guarda il giocatore
-                // Quaternion targetRotation = Quaternion.LookRotation(player.transform.position - transform.position);
-                // transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * lookAtWeight);
+                animator.SetBool("grounded", true); // Si assume che il nemico sia sempre a terra durante il movimento
+                animator.SetBool("attack", false); // Non attacca mentre si muove
+                animator.SetFloat("Velocity", agent.velocity.magnitude); // Utilizza la velocità dell'agente
             }
-            else if (isChasing)
+        }
+        else
+        {
+            if (!isAttacking)
             {
-                // Entra nello stato di riposo.
-                agent.SetDestination(transform.position);
-                isChasing = false;
+                StartCoroutine(PerformAttack());
             }
-
-            // Aggiorna i parametri dell'animatore
-            animator.SetBool("grounded", true); // Si assume che il nemico sia sempre a terra durante il movimento
-            animator.SetBool("attack", isChasing && distanceToPlayer <= attackDistance); // Attacca quando è a distanza di attacco e insegue
-            animator.SetFloat("Velocity", agent.velocity.magnitude); // Utilizza la velocità dell'agente
         }
     }
-
-    void OnTriggerExit(Collider other)
+    else
     {
-        if (other.gameObject == player)
-        {
-            agent.SetDestination(transform.position);
-            isChasing = false;
-        }
+        isAttacking = false;
     }
 }
+
+    IEnumerator PerformAttack()
+    {
+        isAttacking = true;
+
+        agent.isStopped = true;
+
+        animator.SetBool("grounded", true); // Si assume che il nemico sia sempre a terra durante l'attacco
+        animator.SetBool("attack", true);
+        animator.SetFloat("Velocity", 0f); // Nessuna velocità durante l'attacco
+
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length + .5f);
+
+        agent.isStopped = false;
+
+        isAttacking = false;
+    }
+}
+
