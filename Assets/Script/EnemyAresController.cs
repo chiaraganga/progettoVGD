95045e88847e8ca8773e9218bf1183454db5b2d7
albimann <133178849+albimann@users.noreply.github.com>
2023-07-03 13:53:17 +0200

    ho aumentato leggermente il tempo di ritardo prima della fine dell'animazione e ho sistemato lo scudo e il box collider di Ares. Sembra funzionare.
    ho aumentato leggermente il tempo di ritardo prima della fine dell'animazione d'attacco (+ .5f) perché a volte le animazioni possono richiedere più tempo rispetto alla loro lunghezza dichiarata.

--- Assets/Script/EnemyAresController.cs
+++ Assets/Script/EnemyAresController.cs
@@ -5,53 +5,90 @@ using UnityEngine.AI;
 
 public class EnemyAresController : MonoBehaviour
 {
-    public GameObject player; // Assign your player here.
+    public GameObject player; // Assegna qui il tuo giocatore.
     private NavMeshAgent agent;
     private Animator animator;
-    private float attackDistance = 2.5f; // Define how close the enemy needs to be to attack. Modify this value as needed.
-    private GameObject ares;
-    void Start()
-    {
-        gameObject.SetActive(false);
-        
-        // Ensure the GameObject has a NavMeshAgent and Animator.
-        agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
+    private float attackDistance = 2.5f; // Definisci la distanza a cui il nemico deve essere per attaccare. Modifica questo valore se necessario.
+    private float stoppingDistance = 3f; // Definisci la distanza a cui il nemico si ferma per attaccare. Modifica questo valore se necessario.
+    private bool isAttacking = false; // Stato di attacco del nemico
 
-        if (agent == null && animator == null && player == null)
-        {
-            Debug.LogError("Critical component missing from enemy or player not assigned.");
-            return;
-        }
+    void Start()
+{
+    gameObject.SetActive(false);
+
+    // Assicurati che il GameObject abbia un NavMeshAgent e un Animator.
+    agent = GetComponent<NavMeshAgent>();
+    animator = GetComponent<Animator>();
+
+    if (agent == null || animator == null || player == null)
+    {
+        Debug.LogError("Componenti critici mancanti dal nemico o il giocatore non è stato assegnato.");
+        return;
     }
+
+    agent.stoppingDistance = stoppingDistance;
+    agent.obstacleAvoidanceType = ObstacleAvoidanceType.NoObstacleAvoidance; // Disabilita l'evitamento degli ostacoli
+}
+
 
     void Update()
     {
         if (player != null)
         {
             agent.transform.LookAt(player.transform.position);
-            // Calculate distance to player
+            // Calcola la distanza dal giocatore
             float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
-            
-            if (distanceToPlayer > attackDistance)
-            {
-                // Chase the player.
-                agent.SetDestination(player.transform.position);
 
-                // Update animator parameters
-                animator.SetBool("grounded", true); // Assuming the enemy is always on the ground while moving
-                animator.SetBool("attack", false); // Not attacking while moving
-                animator.SetFloat("Velocity", agent.velocity.magnitude); // Use the agent's velocity
+            if (distanceToPlayer > stoppingDistance)
+            {
+                // Insegue il giocatore se non sta attaccando.
+                if (!isAttacking)
+                {
+                    agent.SetDestination(player.transform.position);
+                }
+
+                // Aggiorna i parametri dell'animator
+                animator.SetBool("grounded", true); // Si assume che il nemico sia sempre a terra durante il movimento
+                animator.SetBool("attack", false); // Non attacca mentre si muove
+                animator.SetFloat("Velocity", agent.velocity.magnitude); // Utilizza la velocità dell'agente
             }
             else
             {
-                agent.transform.LookAt(player.transform.position);
-                agent.SetDestination(transform.position);
-                // Attack the player
-                animator.SetBool("grounded", true); // Assuming the enemy is always on the ground while attacking
-                animator.SetBool("attack", true);
-                animator.SetFloat("Velocity", 0); // No velocity while attacking
+                // Ferma il nemico e attacca il giocatore solo se non sta già attaccando
+                if (!isAttacking)
+                {
+                    StartCoroutine(PerformAttack());
+                }
             }
         }
+        else
+        {
+            // Il giocatore non è presente, quindi il nemico non deve attaccare
+            // Ripristina lo stato di attacco su false
+            isAttacking = false;
+        }
+    }
+
+    IEnumerator PerformAttack()
+    {
+        // Imposta lo stato di attacco su true
+        isAttacking = true;
+
+        // Ferma l'agente durante l'attacco
+        agent.isStopped = true;
+
+        // Aggiorna i parametri dell'animator per avviare l'animazione di attacco
+        animator.SetBool("grounded", true); // Si assume che il nemico sia sempre a terra durante l'attacco
+        animator.SetBool("attack", true);
+        animator.SetFloat("Velocity", 0); // Nessuna velocità durante l'attacco
+
+        // Attendi la fine dell'animazione di attacco
+          yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length + .5f);
+
+        // Riavvia l'agente dopo l'attacco
+        agent.isStopped = false;
+
+        // Ripristina lo stato di attacco su false
+        isAttacking = false;
     }
 }
