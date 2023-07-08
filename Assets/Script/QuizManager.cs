076f92282b29944e315e20a60f8c601fabeb4389
Chiara <chiara.ganga98@gmail.com>
2023-07-08 18:05:35 +0200

    commenti script
    inseriti commenti script

--- Assets/Script/QuizManager.cs
+++ Assets/Script/QuizManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 
 public class QuizManager : MonoBehaviour
 {
+    //Variabili
     public List<QuestionHandler> QnA;
     public GameObject[] options;
     public int currentQuestion;
@@ -26,36 +27,34 @@ public class QuizManager : MonoBehaviour
 
     private void Start()
     {
-        Debug.Log("PARTE");
+        
         // totalQuestions = QnA.Count;
 
         if (QnAReply.Count == 0) //inizializzare una sola volta
         for(int i=0; i<QnA.Count; i++) {
-            QnAReply.Add(QnA[i].Clone());
+              //Scorro tutti gli elementi della lista
+              QnAReply.Add(QnA[i].Clone());
                 
-            }
+        }
 
-        GoPanel.SetActive(false);
+        //Disattivo il panello del punteggio
+        GoPanel.SetActive(false); 
         score = 0;
+
+        //Chiamo la funzione per generare le domande
         generateQuestion();
 
-        //Parte che non faceva funzionare 
-       /* trigger_counter++;
-        if (trigger_counter != 1)
-        {
-            QnA.RemoveAt(currentQuestion);
-            trigger_counter = 0;
-        }
-       */
-        //generateQuestion();
+        //Visibile il cursore
         Cursor.visible = true;
     
     }
 
     public void correct()
     {
+        //Aumento il punteggio
         score += 1;
 
+        //Se il punteggio è 4 (quindi max) disattivo il quiz
         if (score >= 4)
         {
             //quizCompleted = true;
@@ -66,6 +65,8 @@ public class QuizManager : MonoBehaviour
         else
         {
            // generateQuestion();
+
+            //Chiamo startTimer
             StartTimer();
         }
     }
@@ -83,16 +84,19 @@ public class QuizManager : MonoBehaviour
     {
 
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        //Richiamo la lista delle domande per effettuare il retry
         for (int i = 0; i < QnAReply.Count; i++)
         {
             QnA.Add(QnAReply[i].Clone());
         }
-        Debug.Log(QnAReply.Count);
+        
         Quizpanel.SetActive(true);
         Start();
 
     }
 
+    //Funzione per stampare il punteggio
     public void GameOver()
     {
         Quizpanel.SetActive(false);
@@ -101,16 +105,16 @@ public class QuizManager : MonoBehaviour
 
     }
 
-
+    //Funzione per generare una domanda
     void generateQuestion()
     {
-        Debug.Log(score);
+        
         if (QnA.Count > 0)
         {
             currentQuestion = Random.Range(0, QnA.Count);
             QuestionTxt.text = QnA[currentQuestion].Question;
             SetAnswer();
-         //   StartTimer();
+         // StartTimer();
 
             // Rimuovi la domanda corrente dalla lista QnA
             QnA.RemoveAt(currentQuestion);
@@ -118,13 +122,14 @@ public class QuizManager : MonoBehaviour
         }
         else
         {
-            Debug.Log("No Question");
+            
             GameOver();
+
+            //Se il punteggio è 4 quindi max, disattiva il pannello
             if (score == 4)
             {
                 Quizpanel.SetActive(false);
                 GoPanel.SetActive(false);
-            
 
             }
 
@@ -133,7 +138,14 @@ public class QuizManager : MonoBehaviour
     }
 
    public void StartTimer()
-    {
+   {
+        /* Controllo che la Corutine non sia nulla
+         * Se la variabile "timerCoroutine" non è nulla,
+         * viene chiamato StopCoroutine per interrompere la coroutine corrente.
+         * Questo è necessario per evitare l'esecuzione simultanea
+         * di più coroutine che possono causare conflitti o errori.
+         */
+
         if (timerCoroutine != null)
         {
             StopCoroutine(timerCoroutine);
@@ -141,14 +153,14 @@ public class QuizManager : MonoBehaviour
 
         timerCoroutine = StartCoroutine(TimerCoroutine());
 
-    }
+   }
 
     private IEnumerator TimerCoroutine()
     {
         yield return new WaitForSeconds(1f);
 
         // Azioni da eseguire al termine del timer
-        Debug.Log("Timer completato!");
+       // Debug.Log("Timer completato!");
 
         // Resetta la variabile del riferimento alla coroutine
         timerCoroutine = null;
@@ -156,9 +168,20 @@ public class QuizManager : MonoBehaviour
         generateQuestion();
     }
 
+    /*
+     Viene impostato "isCorrect" dell'oggetto "AnswersScript" associato all'elemento
+     su "false". Questo indica che la risposta non è corretta di default.
+     Viene impostato il testo della rispettiva risposta nell'oggetto "QnA" relativo
+     alla domanda corrente. La variabile "currentQuestion" rappresenta l'indice
+     della domanda corrente nell'array "QnA".
+     Se l'indice dell'opzione corrente più uno è uguale alla risposta corretta di
+     "QnA" relativo alla domanda corrente, allora "isCorrect" dell'oggetto = "true".
+     Questo indica che è la risposta corretta.
+     */
+
     void SetAnswer()
     {
-
+        // Scorre gli elementi dell'array
         for (int i = 0; i < options.Length; i++)
         {
             options[i].GetComponent<AnswersScript>().isCorrect = false;
@@ -168,6 +191,8 @@ public class QuizManager : MonoBehaviour
             {
                 options[i].GetComponent<AnswersScript>().isCorrect = true;
             }
+
+            /* Il colore dell'immagine dell'elemento viene impostato sul colore di partenza */
 
             options[i].GetComponent<Image>().color = options[i].GetComponent<AnswersScript>().startColor;
         }
