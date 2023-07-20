c72ec7c6abef0f62ea7d8d450065396a573ef38d
albimann <133178849+albimann@users.noreply.github.com>
2023-07-20 13:58:12 +0200

    Finalmente risolti i bug della morte dei nemici nel livello 1!!!!!!!

--- Assets/Script/EnemyHandler.cs
+++ Assets/Script/EnemyHandler.cs
@@ -10,17 +10,13 @@ public class EnemyHandler : MonoBehaviour
     private Animator animator;
     private float attackDistance = 2f;
     private float stoppingDistance = 2f;
-    private bool isAttacking = false;
-    private bool isPerformingAttack = false;
 
-    private bool isIdle = false;
     private float idleDistance = 10f;
     private float followRange = 9f;
     private float originalYPosition;
 
     public Health_manager healthManager;
     private bool isDying = false;
-
 
     void Start()
     {
@@ -40,188 +36,96 @@ public class EnemyHandler : MonoBehaviour
 
     void Update()
     {
-        animator.SetBool("isIdle", isIdle);
         if (player != null)
         {
             if (healthManager.death && !isDying)
             {
-                animator.SetBool("death", true);  // Questo dovrebbe iniziare l'animazione della morte
-                return;  // Usciamo da Update se l'entità è morta
+                StartDeath();
             }
-                if (player != null && healthManager.death && !isDying)
-                {
-                    StartDeath();
-                }
-                else if (player != null && !healthManager.death)
-                {
-                    float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
-
-                    if (isAttacking)
-                    {
-                        if (distanceToPlayer > attackDistance)
-                        {
-                            StopAttack();
-                        }
-                    }
-                    else
-                    {
-                        if (distanceToPlayer <= attackDistance && !isPerformingAttack && !healthManager.death)
-                        {
-                            isIdle = false;
-                            StartAttack();
-                        }
-                        else if (distanceToPlayer <= followRange && !healthManager.death)
-                        {
-                            isIdle = false;
-                            StartChase();
-                        }
-                        else if (distanceToPlayer > idleDistance && !healthManager.death)
-                        {
-                            StartIdle();
-                        }
-                    }
-
-                    if (!isPerformingAttack && agent.velocity.magnitude > 0)
-                    {
-                        LookAtDirection(agent.velocity.normalized);
-                    }
-                    else if (isAttacking)
-                    {
-                        LookAtPlayer();
-                    }
-                }
+            else if (!healthManager.death && !isDying)
+            {
+                HandleMovementAndAttacking();
+            }
         }
-       transform.position = new Vector3(transform.position.x, originalYPosition, transform.position.z);
         
+        if (!isDying)
+        {
+            transform.position = new Vector3(transform.position.x, originalYPosition, transform.position.z);
+        }
+    }
+
+    void HandleMovementAndAttacking()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        animator.SetBool("isIdle", false);
+        if (distanceToPlayer <= attackDistance)
+        {
+            StartAttack();
+        }
+        else if (distanceToPlayer <= followRange)
+        {
+            StartChase();
+        }
+        else if (distanceToPlayer > idleDistance)
+        {
+            StartIdle();
+        }
+
+        if (agent.velocity.magnitude > 0 && !isDying)
+        {
+            LookAtDirection(agent.velocity.normalized);
+        }
     }
 
     void StartAttack()
     {
-        if (healthManager.death)
-            return;
-
-        //Debug.Log("Inizio attacco.");
-        isAttacking = true;
-        StartCoroutine(PerformAttack());
+        agent.isStopped = true;
+        animator.SetBool("attack", true);
+        animator.SetFloat("Velocity", 0f);
     }
 
-    void StopAttack()
+    void StartChase()
     {
-        if (healthManager.death)
-            return;
-
-        //Debug.Log("Fermo attacco.");
-        isAttacking = false;
-        isPerformingAttack = false;
-        agent.velocity = Vector3.zero;
+        agent.isStopped = false;
+        agent.SetDestination(player.transform.position);
         animator.SetBool("attack", false);
-        agent.updatePosition = true;
+        animator.SetFloat("Velocity", agent.velocity.magnitude);
+    }
+
+    void StartIdle()
+    {
+        agent.isStopped = true;
+        animator.SetBool("attack", false);
+        animator.SetFloat("Velocity", 0f);
+        animator.SetBool("isIdle", true);
+    }
+
+    void StartDeath()
+    {
+        isDying = true;
+        agent.SetDestination(transform.position);
+        animator.SetBool("attack", false);  
+        animator.SetFloat("Velocity", 0);
+        animator.SetBool("death", true);  
+        healthManager.death = true; 
     }
 
     void LookAtDirection(Vector3 direction)
     {
-        if (!isAttacking && !healthManager.death)
+        if (!healthManager.death && !isDying)
         {
             Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
         }
     }
 
-    void StartChase()
-    {
-        if (healthManager.death)
-            return;
-
-        //Debug.Log("Inizio inseguimento.");
-        agent.isStopped = false;
-        agent.SetDestination(player.transform.position);
-        animator.SetBool("grounded", true);
-        animator.SetBool("attack", false);
-        animator.SetFloat("Velocity", agent.velocity.magnitude);
-        agent.updateRotation = false;
-    }
-
-    void StartIdle()
-    {
-        if (healthManager.death)
-            return;
-
-        //Debug.Log("Inizio stato idle.");
-        agent.isStopped = true;
-        animator.SetBool("grounded", true);
-        animator.SetBool("attack", false);
-        animator.SetFloat("Velocity", 0f);
-        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
-        agent.updateRotation = false;
-        isIdle = true; // Settiamo isIdle a true quando il nemico entra in stato idle
-    }
-
-void StartDeath()
-{
-    Debug.Log("Inizio morte.");
-    isDying = true;
-    isAttacking = false;
-    agent.SetDestination(transform.position);
-    animator.SetBool("grounded", true);
-    animator.SetBool("attack", false);  
-    animator.SetFloat("Velocity", 0);
-    animator.SetBool("death", true);  
-    healthManager.death = true; 
-}
-
-
     void LookAtPlayer()
     {
-        if (!healthManager.death)
+        if (!healthManager.death && !isDying)
         {
             Vector3 direction = (player.transform.position - transform.position).normalized;
             Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
         }
     }
-
-    // Coroutine per eseguire l'attacco
-    // Coroutine per eseguire l'attacco
-    // Coroutine per eseguire l'attacco
-    IEnumerator PerformAttack()
-{
-
-    if (healthManager.death)
-    {
-        animator.SetBool("death", true);
-        yield break;
-    }
-    
-    if(!healthManager.death)
-    {
-        //Debug.Log("Eseguo attacco.");
-        isPerformingAttack = true;  // Impostiamo il flag di attacco
-        agent.isStopped = true;  // Fermiamo l'agente
-        agent.updatePosition = false;  // Impediamo all'agente di aggiornare automaticamente la sua posizione
-
-        animator.SetBool("grounded", true);  // Impostiamo l'animazione a terra
-        animator.SetBool("attack", true);  // Impostiamo l'animazione di attacco a true
-        animator.SetFloat("Velocity", 0f);  // Impostiamo la velocità dell'animazione a 0
-
-        // Calcoliamo la distanza dal nemico al giocatore
-        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
-
-        // Se la distanza al giocatore è minore o uguale alla distanza di attacco
-            if (distanceToPlayer <= attackDistance)
-            {
-                // Aspettiamo la durata dell'animazione di attacco più un po' di tempo extra
-                yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length + .9f);
-                agent.isStopped = false;  // Facciamo ripartire l'agente
-
-                StopAttack(); // Interrompiamo l'attacco dopo che l'animazione è completata
-
-                yield return new WaitForSeconds(0.5f); // Diamo un po' di tempo prima di un altro attacco
-            }
-
-        isPerformingAttack = false;  // Resettiamo il flag di attacco
-    }
-    
-    
-}
-
 }
--- Assets/Script/Health_manager.cs
+++ Assets/Script/Health_manager.cs
@@ -47,10 +47,10 @@ public void Update()
             {
                 Debug.Log(gameObject.name + " non è morto (death == false)");
                 death = true; // Il personaggio è morto
-                anim.SetBool("dead", true); // Impostare il parametro "death" nell'Animator per avviare l'animazione di morte
+                anim.SetBool("death", true); // Impostare il parametro "death" nell'Animator per avviare l'animazione di morte
                 isDying = true; // Il personaggio sta entrando nella fase di animazione di morte
                 Debug.Log("Chiamata a Invoke per " + gameObject.name);
-                Invoke("CompleteDeathAnimation", 4.4f); // Aggiungere un ritardo per completare l'animazione di morte
+                Invoke("CompleteDeathAnimation", 8f); // Aggiungere un ritardo per completare l'animazione di morte
             }
             else
             {
