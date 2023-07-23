c15c9e89f4be963ff723df4f2cbf6a4e1e6577e6
Chiara <chiara.ganga98@gmail.com>
2023-07-24 00:09:26 +0200

    modifiche
    aggiunti crediti menu principale, modificato "tasti"

--- Assets/Script/Zeus_Dialogue.cs
+++ Assets/Script/Zeus_Dialogue.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using static UnityEditor.Experimental.GraphView.GraphView;
+//using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Zeus_Dialogue : MonoBehaviour
 {
